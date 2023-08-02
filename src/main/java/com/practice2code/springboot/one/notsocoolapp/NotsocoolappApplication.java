14435ed5376afafa11dad2cce72b5babf44d61aa
Entry-Level-Findocx <ejcabral1616@gmail.com>
2023-08-02 09:17:25 +0800

    initial commit

---
+++ src/main/java/com/practice2code/springboot/one/notsocoolapp/NotsocoolappApplication.java
@@ -0,0 +1,13 @@
+package com.practice2code.springboot.one.notsocoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotsocoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotsocoolappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice2code/springboot/one/notsocoolapp/NotsocoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice2code.springboot.one.notsocoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotsocoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
